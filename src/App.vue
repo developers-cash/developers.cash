83a529285b36f7c8851386244a5ca40dce2cd5e1
James Zuccon <zuccon@gmail.com>
2020-11-23 21:54:59 +1100

    Bug fix

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,7 @@
 </template>
 
 <script>
-import Axios from 'axios'
-import AppModule from './store/app'
-import Resources from '../resources.json'
-
 export default {
-  name: 'App',
-  async preFetch ({ store, currentRoute }) {
-    store.commit('app/setResources', Resources);
-    return true;
-  },
+  name: 'App'
 }
 </script>
